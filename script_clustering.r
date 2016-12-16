b1e0c4875cee3547b5b55929e40cb9c98356c25d
SamyAB <aitb.samy@gmail.com>
2016-12-16 18:30:40 +0100

    Ajout des algorithme SOM et du k-means dessus

--- script_clustering.r
+++ script_clustering.r
@@ -83,6 +83,13 @@ SOM_1010 = som(as.matrix(electricite),grid = somgrid(10,10,'rectangular'))
 plot(SOM_1010,type = "quality")
 plot(SOM_1010, type = "dist.neighbours")
 
+#On ne voit pas de rééle frontière sur la distance plot, la encore un k-means sur le SOM
+#On essaye d'utiliser k=3
+KM_SOM = kmeans(SOM_1010$codes,3)
+plot(x= 1:168, y= as.matrix(KM_SOM$centers)[1,],type = 'l',col = 1)
+lines(x=1:168, y =as.matrix(KM_SOM$centers)[2,],type = 'l',col = 2)
+lines(x=1:168, y =as.matrix(KM_SOM$centers)[3,],type = 'l',col = 3)
+
 ############## Jours ouvrables uniquement ################
 #Suffit d'enlever les dernières 48 heures représetant le samedi 06/11/2010 et le dimanche 07/11/2010
 electricite_JO = electricite[,1:120] 
@@ -123,14 +130,14 @@ centers_CAH_2_JO[1,] = centers_CAH_2_JO[1,] / nb_c1_JO
 centers_CAH_2_JO[2,] = centers_CAH_2_JO[2,] / nb_c2_JO
 
 #Dessin des courbes pour les deux centres
-plot(x = 1:168, y = centers_CAH_2_JO[2,] ,type = 'l' , col = 2)
-lines(x = 1:168, y = centers_CAH_2_JO[1,] ,type = 'l' , col = 1)
+plot(x = 1:120, y = centers_CAH_2_JO[2,] ,type = 'l' , col = 2)
+lines(x = 1:120, y = centers_CAH_2_JO[1,] ,type = 'l' , col = 1)
 
 #Pour 3 clusters
 classes_CAH3_JO <- cutree(h_ward, k=2)
 
 ##Création d'une matrice de centres de classes 
-centers_CAH_3_JO = matrix(data = 0, nrow = 3 , ncol = 168) #Matrice contenant les deux centres des deux clusters
+centers_CAH_3_JO = matrix(data = 0, nrow = 3 , ncol = 120) #Matrice contenant les deux centres des deux clusters
 nb3_c1_JO = 0 #Nombre d'éléments du cluster 1
 nb3_c2_JO = 0 #Nombre d'éléments du cluster 2
 nb3_c3_JO = 0 #Nombre d'éléments du cluster 3
@@ -155,38 +162,38 @@ centers_CAH_3_JO[2,] = centers_CAH_3_JO[2,] / nb3_c2_JO
 centers_CAH_3_JO[3,] = centers_CAH_3_JO[3,] / nb3_c3_JO
 
 #Dessin des courbes pour les trois centres
-plot(x = 1:168, y = centers_CAH_2_JO[2,] ,type = 'l' , col = 2)
-lines(x = 1:168, y = centers_CAH_2_JO[1,] ,type = 'l' , col = 1)
-lines(x = 1:168, y = centers_CAH_2_JO[3,] ,type = 'l' , col = 3)
+plot(x = 1:120, y = centers_CAH_3_JO[2,] ,type = 'l' , col = 2)
+lines(x = 1:120, y = centers_CAH_3_JO[1,] ,type = 'l' , col = 1)
+lines(x = 1:120, y = centers_CAH_3_JO[3,] ,type = 'l' , col = 3)
 
 
 ############ K-means Jours Ouvrables ################
 #N'ayant pas de critère à priori pour le nombre de clusters, on test de k dans [2,5]
 # K = 2
 KM_2_JO = kmeans(electricite_JO,2)
-plot(x = 1:168, y=as.matrix(KM_2_JO$centers)[1,],type = 'l',col = 1)
-lines(x=1:168, y =as.matrix(KM_2_JO$centers)[2,],type = 'l',col = 2)
+plot(x = 1:120, y=as.matrix(KM_2_JO$centers)[1,],type = 'l',col = 1)
+lines(x=1:120, y =as.matrix(KM_2_JO$centers)[2,],type = 'l',col = 2)
 KM_2_JO$tot.withinss
 # K = 3 
 KM_3_JO = kmeans(electricite_JO,3)
-plot(x = 1:168, y=as.matrix(KM_3_JO$centers)[2,],type = 'l',col = 2)
-lines(x=1:168, y =as.matrix(KM_3_JO$centers)[1,],type = 'l',col = 1)
-lines(x=1:168, y =as.matrix(KM_3_JO$centers)[3,],type = 'l',col = 3)
+plot(x = 1:120, y=as.matrix(KM_3_JO$centers)[2,],type = 'l',col = 2)
+lines(x=1:120, y =as.matrix(KM_3_JO$centers)[1,],type = 'l',col = 1)
+lines(x=1:120, y =as.matrix(KM_3_JO$centers)[3,],type = 'l',col = 3)
 KM_3_JO$tot.withinss
 # K = 4
 KM_4_JO = kmeans(electricite_JO,4)
-plot(x = 1:168, y=as.matrix(KM_4_JO$centers)[3,],type = 'l',col = 3)
-lines(x=1:168, y =as.matrix(KM_4_JO$centers)[1,],type = 'l',col = 1)
-lines(x=1:168, y =as.matrix(KM_4_JO$centers)[4,],type = 'l',col = 4)
-lines(x=1:168, y =as.matrix(KM_4_JO$centers)[2,],type = 'l',col = 2)
+plot(x = 1:120, y=as.matrix(KM_4_JO$centers)[3,],type = 'l',col = 3)
+lines(x=1:120, y =as.matrix(KM_4_JO$centers)[1,],type = 'l',col = 1)
+lines(x=1:120, y =as.matrix(KM_4_JO$centers)[4,],type = 'l',col = 4)
+lines(x=1:120, y =as.matrix(KM_4_JO$centers)[2,],type = 'l',col = 2)
 KM_4_JO$tot.withinss
 # K = 5
 KM_5_JO = kmeans(electricite_JO,5)
-plot(x = 1:168, y=as.matrix(KM_5_JO$centers)[3,],type = 'l',col = 3)
-lines(x=1:168, y =as.matrix(KM_5_JO$centers)[1,],type = 'l',col = 1)
-lines(x=1:168, y =as.matrix(KM_5_JO$centers)[4,],type = 'l',col = 4)
-lines(x=1:168, y =as.matrix(KM_5_JO$centers)[2,],type = 'l',col = 2)
-lines(x=1:168, y =as.matrix(KM_5_JO$centers)[5,],type = 'l',col = 5)
+plot(x = 1:120, y=as.matrix(KM_5_JO$centers)[3,],type = 'l',col = 3)
+lines(x=1:120, y =as.matrix(KM_5_JO$centers)[1,],type = 'l',col = 1)
+lines(x=1:120, y =as.matrix(KM_5_JO$centers)[4,],type = 'l',col = 4)
+lines(x=1:120, y =as.matrix(KM_5_JO$centers)[2,],type = 'l',col = 2)
+lines(x=1:120, y =as.matrix(KM_5_JO$centers)[5,],type = 'l',col = 5)
 KM_5_JO$tot.withinss
 
 ############### SOM Jours Ouvrables #####################
@@ -194,4 +201,9 @@ SOM_1010_JO = som(as.matrix(electricite_JO),grid = somgrid(10,10,'rectangular'))
 plot(SOM_1010_JO,type = "quality")
 plot(SOM_1010_JO, type = "dist.neighbours")
 
-
+#On ne voit pas de rééle frontière sur la distance plot, la encore un k-means sur le SOM
+#On essaye d'utiliser k=3
+KM_SOM_JO = kmeans(SOM_1010_JO$codes,3)
+plot(x= 1:120, y= as.matrix(KM_SOM_JO$centers)[1,],type = 'l',col = 1)
+lines(x=1:120, y =as.matrix(KM_SOM_JO$centers)[2,],type = 'l',col = 2)
+lines(x=1:120, y =as.matrix(KM_SOM_JO$centers)[3,],type = 'l',col = 3)
